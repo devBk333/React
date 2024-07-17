c5bd7b0c97e3a21ce00f805c8d64e934dd15884f
Helson <helsonplay20@gmail.com>
2024-07-17 16:21:47 -0300

    CSS

--- My-ap/src/App.js
+++ My-ap/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import HelloWord from './componentes/HelloWord'
 import SayMyName from './componentes/SayMyName'
 import Pessoa from './componentes/Pessoa'
+import Frase from './componentes/Frase'
 
 function App() {
   
   //div  container conteudo   //Classname no react
   return (
     <div className="App">      
+      <Frase/>
       <HelloWord/>
       <SayMyName nome= "Helson"/>
       <Pessoa 
