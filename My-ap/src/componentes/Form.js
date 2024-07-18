06a1e6edc8acb7e240997b18448c5235b18c1589
Helson <helsonplay20@gmail.com>
2024-07-18 15:26:25 -0300

    Hook State

--- My-ap/src/componentes/Form.js
+++ My-ap/src/componentes/Form.js
@@ -1,18 +1,37 @@
 //formularios
+import { useState } from 'react'         //import State
 
 function Form(){
 
     function cadastrarUsuario(e) {
         e.preventDefault() //para html e faz isso primeiro  precisa quando tiver evento
-        console.log('Cadastrou usuário!')
-    }
+        console.log(name)  //console
+        console.log('Cadastrou usuário!') }
+
+    const [name, setName] = useState()            //implementaçao hook  State  
+    const [password, setPassword] = useState()    //cria constante   
 
     return(
         <div>
             <h1>Meu Cadastro</h1>
             <form onSubmit = {cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome"/>
+                <label htmlFor="name">Nome:</label> 
+                <input 
+                 type="text" 
+                 id="name"
+                 name="name"
+                 placeholder="Digite seu nome"                        //set as const baseado em campo de input
+                 onChange={(e) => setName(e.target.value)}/>  
+                </div>
+                <div>
+                <label htmlFor="name">Senha:</label>
+                <input 
+                 type="password"
+                 id="password"
+                 name="password"
+                 placeholder="Digite sua senha"
+                 onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"/>
